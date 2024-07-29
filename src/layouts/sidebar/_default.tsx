75988e88df837e03ec3fa2452e1c92b0b9f19e2d
Thamer Dridi <thamer.dridi@etudiant-isi.utm.tn>
2024-07-29 11:28:06 +0200

    first commit to dev

---
+++ src/layouts/sidebar/_default.tsx
@@ -0,0 +1,139 @@
+import cn from 'classnames';
+import AuthorCard from '@/components/ui/author-card';
+import Logo from '@/components/ui/logo';
+import { MenuItem } from '@/components/ui/collapsible-menu';
+import Scrollbar from '@/components/ui/scrollbar';
+import Button from '@/components/ui/button';
+import routes from '@/config/routes';
+import { useDrawer } from '@/components/drawer-views/context';
+import { HomeIcon } from '@/components/icons/home';
+import { ProfileIcon } from '@/components/icons/profile';
+import { DiskIcon } from '@/components/icons/disk';
+import { PlusCircle } from '@/components/icons/plus-circle';
+import { CompassIcon } from '@/components/icons/compass';
+import { Close } from '@/components/icons/close';
+
+//images
+import AuthorImage from '@/assets/images/author.jpg';
+
+export const menuItems = [
+  {
+    name: 'Welcome',
+    icon: <HomeIcon />,
+    href: routes.welcome,
+  },
+ /*  {
+    name: 'Explore NFTs',
+    icon: <CompassIcon />,
+    href: routes.search,
+  },
+  {
+    name: 'Create NFT',
+    icon: <PlusCircle />,
+    href: routes.createNft,
+  },
+  {
+    name: 'NFT Details',
+    icon: <DiskIcon />,
+    href: routes.nftDetails,
+  },
+  {
+    name: 'Profile',
+    icon: <ProfileIcon />,
+    href: routes.profile,
+  }, */
+  {
+    name: 'Create Project',
+    icon: <PlusCircle />,
+    href: routes.createProject,
+  },
+  {
+    name: 'Create MRV',
+    icon: <PlusCircle />,
+    href: routes.createMrv,
+  },
+  {
+    name: 'VVB Profile',
+    icon: <ProfileIcon />,
+    href: routes.vvbProfile,
+  },
+  {
+    name: 'Farmer Profile',
+    icon: <ProfileIcon />,
+    href: routes.farmerProfile,
+  },
+  {
+    name: 'MRV Explore',
+    icon: <CompassIcon />,
+    href: routes.mrvExplore,
+  },
+  {
+    name: 'MRV Details',
+    icon: <DiskIcon />,
+    href: routes.mrvDetails,
+  },
+  {
+    name: 'Project Details',
+    icon: <DiskIcon />,
+    href: routes.projectDetails,
+  },
+  {
+    name: 'Project Explore',
+    icon: <CompassIcon />,
+    href: routes.projectExplore,
+  }
+];
+
+type SidebarProps = {
+  className?: string;
+};
+
+export default function Sidebar({ className }: SidebarProps) {
+  const { closeDrawer } = useDrawer();
+  return (
+    <aside
+      className={cn(
+        'top-0 z-40 h-full w-full max-w-full border-dashed border-gray-200 bg-body ltr:left-0 ltr:border-r rtl:right-0 rtl:border-l dark:border-gray-700 dark:bg-dark xs:w-80 xl:fixed  xl:w-72 2xl:w-80',
+        className
+      )}
+    >
+      <div className="relative flex h-24 items-center justify-between overflow-hidden px-6 py-4 2xl:px-8">
+        <Logo />
+        <div className="md:hidden">
+          <Button
+            title="Close"
+            color="white"
+            shape="circle"
+            variant="transparent"
+            size="small"
+            onClick={closeDrawer}
+          >
+            <Close className="h-auto w-2.5" />
+          </Button>
+        </div>
+      </div>
+
+      <Scrollbar style={{ height: 'calc(100% - 96px)' }}>
+        <div className="px-6 pb-5 2xl:px-8">
+          <AuthorCard
+            image={AuthorImage}
+            name="Cameron Williamson"
+            role="admin"
+          />
+
+          <div className="mt-12">
+            {menuItems.map((item, index) => (
+              <MenuItem
+                key={index}
+                name={item.name}
+                href={item.href}
+                icon={item.icon}
+               /*  dropdownItems={item.dropdownItems} */
+              />
+            ))}
+          </div>
+        </div>
+      </Scrollbar>
+    </aside>
+  );
+}
